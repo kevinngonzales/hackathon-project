7b4173c39e2ce8b03be58a49fe0a56f6e020f8da
vinson-han <han_vinson@student.smc.edu>
2024-05-05 00:03:32 -0700

    discover

--- hackathon-project/src/functions/langchain.js
+++ hackathon-project/src/functions/langchain.js
@@ -1,23 +1,22 @@
 import { OpenAI } from "@langchain/openai";
 
-
-//variable declared to hold OpenAI's API key stored in env variable.  
+//variable declared to hold OpenAI's API key stored in env variable.
 //then use langchain to manage OpenAI's LLM by creating a new instance of the class using the API key
 //this allows use to pass data through and recieve a response using invoke method
 
-const apiKey = import.meta.env.VITE_OPENAI_API_KEY
+const apiKey = import.meta.env.VITE_OPENAI_API_KEY;
 
 const llm = new OpenAI({
-    openAIApiKey: apiKey,
+  openAIApiKey: apiKey,
 });
 
 export async function getAnswer(question) {
-    let answer = 'Should return an Answer'
-    try {
-        // answer = await llm.invoke(question);
-        console.log(answer);
-        return answer
-    } catch (e) {
-        console.error(e);
-    }
+  let answer = "Should return an Answer";
+  try {
+    answer = await llm.invoke(question);
+    console.log(answer);
+    return answer;
+  } catch (e) {
+    console.error(e);
+  }
 }
